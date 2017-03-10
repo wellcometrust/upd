b0f6cc777e38aedd733e9dd66cc86e0dbcf9c84d
Tim Swalling <tim.swalling@hactar.is>
2017-03-10 16:41:36 +0000

    Add site navigation patterns

---
+++ pattern-library/src/components/02-patterns/site-navigation/site-navigation.config.js
@@ -0,0 +1,14 @@
+const config = {
+  context: {
+    items: [
+      'About us',
+      'Resources',
+      'Supporting conversations',
+      'Case studies',
+      'News',
+      'Contact'
+    ]
+  }
+}
+
+module.exports = config
