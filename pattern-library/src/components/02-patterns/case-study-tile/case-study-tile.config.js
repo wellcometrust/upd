3a0e56c864af2af1d0fcd5c8ca2447af9caf4c55
Tim Swalling <tim.swalling@hactar.is>
2017-03-11 14:11:15 +0000

    Add case study tile pattern

---
+++ pattern-library/src/components/02-patterns/case-study-tile/case-study-tile.config.js
@@ -0,0 +1,16 @@
+const config = {
+  context: {
+    cta: {
+      icon: 'arrow',
+      label: 'View case study',
+      small: true
+    },
+    image: '/images/placeholder/case-study-tile-whooping-cough.jpg',
+    level: '2',
+    tag: 'Disease risks, causes & mechanisms',
+    title: 'Whooping cough vaccination during pregnancy'
+  },
+  preview: '@preview-boxed'
+}
+
+module.exports = config
