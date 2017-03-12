372e2348426f5c2e5c5ee01ee24b596877748636
Tim Swalling <tim.swalling@hactar.is>
2017-03-12 17:12:40 +0000

    Add search results patterns

---
+++ pattern-library/src/components/02-patterns/search-results/search-results.config.js
@@ -0,0 +1,45 @@
+const makeItem = (isCaseStudy) => {
+  return {
+    description: (isCaseStudy)
+      ? 'Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis…'
+      : '…estibulum rutrum quam vitae <strong>iaculis</strong> fringilla tincidunt. Suspendisse nec tortor urna. Ut laoreet sodales nisi, quis <strong>iaculis</strong> nulla <strong>iaculis</strong> vitae. Donec sagittis faucibus lacus eget blandit. Mauris vitae ultricies metus, at condimentum nulla. Donec quis ornare lacus. Etiam gravida mollis <strong>iaculis</strong> tortor quis porttitor…',
+    title: 'Fusce vehicula dolor arcu, sit amet blandit'
+  }
+}
+
+const makeItems = (count, isCaseStudy) => {
+  let items = []
+
+  for (let i = 0; i < count; i++) {
+    let item = makeItem(isCaseStudy)
+
+    if (isCaseStudy) {
+      item.tag = true
+    } else {
+      item.category = 'Content type'
+    }
+
+    items.push(item)
+  }
+
+  return items
+}
+
+const config = {
+  context: {
+    count: 123,
+    items: makeItems(10)
+  },
+  preview: '@preview-boxed',
+  variants: [
+    {
+      context: {
+        featured: true,
+        items: makeItems(7, true)
+      },
+      name: 'Case studies'
+    }
+  ]
+}
+
+module.exports = config
