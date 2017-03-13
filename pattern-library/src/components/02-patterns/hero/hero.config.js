95cb7aea9ce2b521974532a65d8da03f74a5f79b
Tim Swalling <tim.swalling@hactar.is>
2017-03-13 11:08:50 +0000

    Add hero patterns and placeholder background illustration

---
+++ pattern-library/src/components/02-patterns/hero/hero.config.js
@@ -0,0 +1,25 @@
+const config = {
+  context: {
+    intro: 'More than two thirds of the population do not feel they know how health data is used in the NHS. We want to change that. Understanding Patient Data has been set up to support conversations with the public, patients and healthcare professionals about how health and care data is used, providing resources for people to find out more.',
+    'key-info': [
+      {
+        button: 'How data is used',
+        icon: 'key-info-placeholder',
+        text: 'Using patient data is essential to improve health and deliver better care'
+      },
+      {
+        button: 'How it is kept safe',
+        icon: 'key-info-placeholder',
+        text: 'There must be robust safeguards to protect patient confidentiality when data is used'
+      },
+      {
+        button: 'Resources',
+        icon: 'key-info-placeholder',
+        text: 'Everyone should know how patient data is used and why, and what choices you can make'
+      }
+    ],
+    title: 'Understanding Patient Data'
+  }
+}
+
+module.exports = config
