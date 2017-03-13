83b725d1e9ef5cdb60038c6556c1476da77d2512
Tim Swalling <tim.swalling@hactar.is>
2017-03-13 09:38:18 +0000

    A key information tile patterns and placeholder icon

---
+++ pattern-library/src/components/02-patterns/key-info-tile/key-info-tile.config.js
@@ -0,0 +1,10 @@
+const config = {
+  context: {
+    button: 'How data is used',
+    icon: 'key-info-placeholder',
+    text: 'Using patient data is essential to improve health and deliver better care'
+  },
+  preview: '@preview-boxed'
+}
+
+module.exports = config
