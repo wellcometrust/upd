346b6d6d45d920e078166caf141eca769244be02
Tim Swalling <tim.swalling@hactar.is>
2017-03-14 14:56:08 +0000

    Add intro pattern

---
+++ pattern-library/src/components/02-patterns/intro/intro.config.js
@@ -0,0 +1,8 @@
+const config = {
+  context: {
+    content: '<p>The current vocabulary for the use of patient data in care, treatment and research is difficult, complex and confusing.  Many different words are used to describe the same thing, and many of those words are unnecessarily technical (for example pseudonymised, key-coded, de-identified for limited disclosure).  This acts as a significant barrier to discussing the use of data with the public in ways that can build both understanding and confidence.</p><p>We think that an important part of improving conversations about patient data is getting the words right, in a way that is accurate but also accessible and meaningful. If everyone is on the same page in using these words, it’ll be easier for clinicians, patients, researchers and the public to have informed discussions about how patient data is used and why.</p>'
+  },
+  preview: '@preview-boxed'
+}
+
+module.exports = config
