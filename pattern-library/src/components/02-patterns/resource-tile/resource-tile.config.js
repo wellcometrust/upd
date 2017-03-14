efe0ba0c342a3e4ae1dda72435d84166fc6d1df8
Tim Swalling <tim.swalling@hactar.is>
2017-03-14 10:35:08 +0000

    Add resource tile pattern

---
+++ pattern-library/src/components/02-patterns/resource-tile/resource-tile.config.js
@@ -0,0 +1,10 @@
+const config = {
+  context: {
+    button: 'Report on safeguards',
+    description: '<p>Patient information is sensitive and confidential. We will only be able to realise the huge potential and value of patient data in the NHS if both the public and clinicians can trust the system, and have confidence that data is appropriately collected and managed</p>',
+    title: 'How is data kept safe?'
+  },
+  preview: '@preview-boxed'
+}
+
+module.exports = config
