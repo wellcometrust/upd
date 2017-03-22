dd404312096f7a6cd016e2ed0737b00600851cb0
Tim Swalling <tim.swalling@hactar.is>
2017-03-22 10:37:17 +0000

    Add accordion only and monolith layout patterns

---
+++ pattern-library/src/components/03-templates/accordion-only-template/accordion-only-template.config.js
@@ -0,0 +1,16 @@
+const config = {
+  context: {
+    content: [
+      {
+        name: 'content--accordion-only'
+      }
+    ],
+    header: {
+      subheading: 'Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at',
+      title: 'Title goes here'
+    },
+    monolith: true
+  }
+}
+
+module.exports = config
