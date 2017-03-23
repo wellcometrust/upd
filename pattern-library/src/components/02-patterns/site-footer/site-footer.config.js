ba0e39fea41ebdb5f138d7df7cbe9b291d1596ab
Tim Swalling <tim.swalling@hactar.is>
2017-03-23 11:23:20 +0000

    Add site footer patterns

---
+++ pattern-library/src/components/02-patterns/site-footer/site-footer.config.js
@@ -0,0 +1,29 @@
+const config = {
+  context: {
+    socials: [
+      {
+        classes: 'site-footer__social-icon',
+        circular: true,
+        href: '#link-to-twitter',
+        icon: 'twitter',
+        title: 'Follow us on Twitter'
+      },
+      {
+        classes: 'site-footer__social-icon',
+        circular: true,
+        href: '#link-to-linkedin',
+        icon: 'linkedin',
+        title: 'Follow us on LinkedIn'
+      },
+      {
+        classes: 'site-footer__social-icon',
+        circular: true,
+        href: '#link-to-facebook',
+        icon: 'facebook',
+        title: 'Follow us on Facebook'
+      }
+    ]
+  }
+}
+
+module.exports = config
