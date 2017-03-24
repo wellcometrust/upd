469c9673b68f8a25c599617109fd1c206587d601
Tim Swalling <tim.swalling@hactar.is>
2017-03-24 17:43:45 +0000

    Add header search JavaScript

--- pattern-library/src/js/index.js
+++ pattern-library/src/js/index.js
@@ -1,7 +1,9 @@
+import { initHeaderSearch } from './header-search'
 import masonry from './masonry'
 import { initScrollSpies } from './scroll-spy'
 import toggle from './toggle'
 
 masonry.init()
 toggle.init()
+initHeaderSearch()
 initScrollSpies()
