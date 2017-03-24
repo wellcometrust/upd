324ba1ce5216a9e740fdbe159f24a7037208a954
Tim Swalling <tim.swalling@hactar.is>
2017-03-24 15:32:49 +0000

    Add contact us patterns

--- pattern-library/src/components/02-patterns/layout/layout.config.js
+++ pattern-library/src/components/02-patterns/layout/layout.config.js
@@ -49,6 +49,12 @@ const config = {
         sticky: false
       },
       name: 'Case studies'
+    },
+    {
+      context: {
+        content: '<p>Contact the team directly on <a class="u-whitespace-nowrap" href="tel:+4420786118646">+44 207 8611 8646</a></p>\n\n<p>or email <a href="mailto:hello@understandingpatientdata.org.uk">hello@understandingpatientdata.org.uk</a></p>'
+      },
+      name: 'Contact us'
     }
   ]
 }
