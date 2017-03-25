607ff085874a2739bd1e46555428b180a7c2d3e8
Tim Swalling <tim.swalling@hactar.is>
2017-03-25 17:17:17 +0000

    Update site header pattern to match new designs

---
+++ pattern-library/src/components/02-patterns/_header-search/header-search.config.js
@@ -0,0 +1,17 @@
+const config = {
+  context: {
+    icons: [
+      {
+        name: 'arrow',
+        classes: 'header-search__icon  header-search__icon--mobile'
+      },
+      {
+        name: 'search',
+        classes: 'header-search__icon  header-search__icon--desktop'
+      }
+    ],
+    title: 'Search'
+  }
+}
+
+module.exports = config
