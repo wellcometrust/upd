26eb80ffa09fce628c776d0d1f2151a55d45a3c9
Tim Swalling <tim.swalling@hactar.is>
2017-03-26 16:22:17 +0100

    Add JavaScript Focus class

--- pattern-library/src/js/index.js
+++ pattern-library/src/js/index.js
@@ -1,9 +1,11 @@
+import { initFocus } from './focus'
 import { initHeaderSearch } from './header-search'
 import masonry from './masonry'
 import { initScrollSpies } from './scroll-spy'
 import { initToggles } from './toggle'
 
 masonry.init()
+initFocus()
 initHeaderSearch()
 initScrollSpies()
 initToggles()
