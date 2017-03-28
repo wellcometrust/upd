b7f00bfb51f2cc06e43d34e38c8ecc0e4b86a784
Tim Swalling <tim.swalling@hactar.is>
2017-03-28 11:15:58 +0100

    Update key info tiles to match design

--- pattern-library/src/components/02-patterns/hero/hero.config.js
+++ pattern-library/src/components/02-patterns/hero/hero.config.js
@@ -3,17 +3,17 @@ const config = {
     intro: 'More than two thirds of the population do not feel they know how health data is used in the NHS. We want to change that. Understanding Patient Data has been set up to support conversations with the public, patients and healthcare professionals about how health and care data is used, providing resources for people to find out more.',
     keyInfo: [
       {
-        button: 'How data is used',
+        cta: 'How data is used',
         icon: 'home-heart',
         text: 'Using patient data is essential to improve health and deliver better care'
       },
       {
-        button: 'How it is kept safe',
+        cta: 'How it is kept safe',
         icon: 'home-shield',
         text: 'There must be robust safeguards to protect patient confidentiality when data is used'
       },
       {
-        button: 'Resources',
+        cta: 'Resources',
         icon: 'home-book',
         text: 'Everyone should know how patient data is used and why, and what choices you can make'
       }
--- pattern-library/src/components/02-patterns/key-info-tile/key-info-tile.config.js
+++ pattern-library/src/components/02-patterns/key-info-tile/key-info-tile.config.js
@@ -1,6 +1,6 @@
 const config = {
   context: {
-    button: 'How data is used',
+    cta: 'How data is used',
     icon: 'home-heart',
     text: 'Using patient data is essential to improve health and deliver better care'
   },
