2fdaf0ad3b93d9d8b841f17e6bfa099029488be2
Pascal <pascal.morin@codeenigma.com>
2017-03-28 15:54:12 +0200

    Update drush aliases

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -1,5 +1,5 @@
 <?php
 
-$ignore = array('devel', 'kint', 'devel_generate', 'webprofiler');
+$ignore = array('devel', 'kint', 'devel_generate', 'webprofiler', 'views_ui', 'page_manager_ui');
 $command_specific['config-export']['skip-modules'] = $ignore;
 $command_specific['config-import']['skip-modules'] = $ignore;
