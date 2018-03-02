72a14a6d429017d94c357393ce1a247ca62732bb
Ana R <ana.rdrg@gmail.com>
2018-03-02 12:29:40 +0000

    Add markup for "media-panel" component along with its variations; add placeholder image;

---
+++ pattern-library/src/components/02-patterns/media-panel/media-panel.config.js
@@ -0,0 +1,20 @@
+const config = {
+  context: {
+    link: 'What else can you do',
+    description: 'Nam dapibus nisl vitae elit fringilla rutrum. Aenean sollicitudin, erat a elementum rutrum, neque sem pretium metus,sollicitudin, erat a elementum rutrum.',
+    title: 'How your data can help others who suffer with cancer'
+  },
+  variants: [
+    {
+      context: {
+        link: 'What else can you do',
+        description: 'Nam dapibus nisl vitae elit fringilla rutrum. Aenean sollicitudin, erat a elementum rutrum, neque sem pretium metus,sollicitudin, erat a elementum rutrum.',
+        title: 'How your data can help others who suffer with cancer',
+        video: true
+      },
+      name: 'Media Panel with Video'
+    }
+  ]
+}
+
+module.exports = config
