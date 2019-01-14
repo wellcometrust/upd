85f85112a9a51cb3e1d5cf743746b7a2887993d5
Luke Whitmore <lwhitmore@gmail.com>
2019-01-14 23:00:40 +0000

    Update to 8.6.5 + contrib updates.

---
+++ www/modules/contrib/simple_sitemap/src/Plugin/simple_sitemap/SitemapGenerator/SitemapWriter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\simple_sitemap\Plugin\simple_sitemap\SitemapGenerator;
+
+use XMLWriter;
+
+/**
+ * Class SitemapWriter
+ * @package Drupal\simple_sitemap\Plugin\simple_sitemap\SitemapGenerator
+ */
+class SitemapWriter extends XMLWriter {
+
+}
