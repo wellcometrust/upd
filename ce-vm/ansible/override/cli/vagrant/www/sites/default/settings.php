ffa99dbef72d4d20e3fc174d915a77b369c6c2ef
Moisés Rodríguez Carmona <moises.rodriguez@codeenigma.com>
2020-04-16 08:08:18 +0200

    49093 Fixed VM and added solr support to it

--- ce-vm/ansible/override/cli/vagrant/www/sites/default/settings.php
+++ ce-vm/ansible/override/cli/vagrant/www/sites/default/settings.php
@@ -5,6 +5,23 @@
  */
 require __DIR__ . '/default.settings.php';
 
+/**
+ * Include dev settings.
+ */
+require DRUPAL_ROOT . '/sites/example.settings.local.php';
+
+/**
+ * Skip file system permissions hardening.
+ *
+ * The system module will periodically check the permissions of your site's
+ * site directory to ensure that it is not writable by the website user. For
+ * sites that are managed with a version control system, this can cause problems
+ * when files in that directory such as settings.php are updated, because the
+ * user pulling in the changes won't have permissions to modify files in the
+ * directory.
+ */
+$settings['skip_permissions_hardening'] = TRUE;
+
 $databases['default']['default'] = array (
   'database' => 'drupal',
   'username' => 'dev',
@@ -17,18 +34,25 @@ $databases['default']['default'] = array (
 );
 
 /**
- * VM settings
+ * @see https://www.drupal.org/project/config_ignore
  */
-$conf['advagg_admin_mode'] = 0;
-$conf['advagg_font_fontfaceobserver'] = 6;
-$conf['advagg_css_compressor'] = -1;
-$conf['advagg_js_compressor'] = 0;
-$conf['advagg_js_compress_inline'] = 0;
+$config['config_ignore.settings']['ignored_config_entities'] = array(
+  'devel.*',
+  'kint.*',
+  'webprofiler.*',
+  'devel_generate.*',
+);
+$settings['hash_salt'] = 'ce-vm';
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+$config_directories['sync'] = '../config/sync';
+$settings['trusted_host_patterns'] = [ '.*' ];
 
-$conf['stage_file_proxy_origin'] = 'https://understandingpatientdata.org.uk';
-$conf['https'] = TRUE;
-
-$conf['cache'] = FALSE;
+// Stage file proxy.
+$config['stage_file_proxy.settings']['origin'] = 'https://understandingpatientdata.org.uk';
 
 /**
  * Include local dev settings if any (gitignored, normally).
@@ -36,5 +60,3 @@ $conf['cache'] = FALSE;
 if (file_exists(__DIR__ . '/settings.local.php')) {
    include __DIR__ . '/settings.local.php';
 }
-
-$conf['drupal_http_request_fails'] = FALSE;
