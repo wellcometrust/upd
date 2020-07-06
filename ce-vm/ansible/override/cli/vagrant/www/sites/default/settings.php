d918c43e63c32d13794d52ab70bd30d6e04bd981
David Suissa <david.suissa@codeenigma.com>
2020-07-07 00:04:13 +0200

    r50405: Additional configuration changes for 'local' and 'master' splits. Implementation of config_split in all ENV.settings.php files.

--- ce-vm/ansible/override/cli/vagrant/www/sites/default/settings.php
+++ ce-vm/ansible/override/cli/vagrant/www/sites/default/settings.php
@@ -1,8 +1,10 @@
 <?php
 
 /**
- * Include default settings.
+ * @file
+ * Include default settings for the local ce-vm development environment.
  */
+
 require __DIR__ . '/default.settings.php';
 
 /**
@@ -22,7 +24,7 @@ require DRUPAL_ROOT . '/sites/example.settings.local.php';
  */
 $settings['skip_permissions_hardening'] = TRUE;
 
-$databases['default']['default'] = array (
+$databases['default']['default'] = [
   'database' => 'drupal',
   'username' => 'dev',
   'password' => 'dev',
@@ -31,32 +33,38 @@ $databases['default']['default'] = array (
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
-);
+];
 
-/**
- * @see https://www.drupal.org/project/config_ignore
- */
-/*$config['config_ignore.settings']['ignored_config_entities'] = array(
-  'devel.*',
-  'kint.*',
-  'webprofiler.*',
-  'devel_generate.*',
-);*/
+// Config sync directory.
+$settings['config_sync_directory'] = '../config/sync';
+
+// Enable the 'local' config_split and force other splits to be disabled.
+$config['config_split.config_split.local']['status'] = TRUE;
+$config['config_split.config_split.master']['status'] = FALSE;
+$config['config_split.config_split.prod']['status'] = FALSE;
+
+// Other settings.
 $settings['hash_salt'] = 'ce-vm';
-$config['system.performance']['css']['preprocess'] = FALSE;
-$config['system.performance']['js']['preprocess'] = FALSE;
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
-$config_directories['sync'] = '../config/sync';
-$settings['trusted_host_patterns'] = [ '.*' ];
+$settings['trusted_host_patterns'] = ['.*'];
 
-// Stage file proxy.
-$config['stage_file_proxy.settings']['origin'] = 'https://understandingpatientdata.org.uk';
+// @TODO: #49093 Fix Solr configuration locally and search engine on website.
+$config['search_api.server.local_solr'] = [
+  'backend_config' => [
+    'connector_config' => [
+      'host' => '192.168.57.130',
+      'path' => '/',
+      'core' => 'upd_core',
+      'port' => '8080',
+    ],
+  ],
+];
 
 /**
  * Include local dev settings if any (gitignored, normally).
  */
 if (file_exists(__DIR__ . '/settings.local.php')) {
-   include __DIR__ . '/settings.local.php';
+  include __DIR__ . '/settings.local.php';
 }
